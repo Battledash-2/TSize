8b2d695277dc0a81c99174a076c44a130e654959
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-02-21 20:01:29 -0500

    make `size` function check the size every time it is called

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const size = require('./').size();
-const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor id nisl in mollis. Sed sed fringilla ante. Quisque tincidunt volutpat dolor a lacinia. Pellentesque dapibus ultrices egestas. Maecenas vehicula eros non felis eleifend sagittis. Cras interdum rutrum faucibus. Vestibulum bibendum dictum urna vel euismod. Vestibulum vel interdum nisl.`;
+const size = require('./');
 
-const linesInText = text.length / size.width;
-console.log('There are', Math.floor(linesInText)+1, 'newlines in text', '\n\n'+text);
+setInterval(()=>{
+	console.log(size.size());
+}, 250);
